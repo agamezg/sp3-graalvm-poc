a6f79b397cc6417686b77c6c036698c2fd8773d7
ariel_prisma <agamez@prismamediosdepago.com>
2024-04-09 13:39:17 -0300

    initial commit

---
+++ src/main/kotlin/org/example/sp3graalvmpoc/Sp3GraalvmPocApplication.kt
@@ -0,0 +1,11 @@
+package org.example.sp3graalvmpoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Sp3GraalvmPocApplication
+
+fun main(args: Array<String>) {
+    runApplication<Sp3GraalvmPocApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/sp3graalvmpoc/Sp3GraalvmPocApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.sp3graalvmpoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Sp3GraalvmPocApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
