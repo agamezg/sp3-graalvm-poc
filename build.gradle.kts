c1a5cd2abaad22229a6a7f8068a6372baa5ee8b5
ariel_prisma <agamez@prismamediosdepago.com>
2024-04-10 01:19:46 -0300

    chore: add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,15 +19,52 @@ repositories {
     mavenCentral()
 }
 
+val springCloudVersion = "2022.0.3"
+val openapiVersion = "2.1.0"
+val arrowVersion = "1.2.0-RC"
+val kotestVersion = "5.5.5"
+val kotestArrowVersion = "1.3.0"
+val mockkVersion = "1.13.4"
+
 dependencies {
+
+    // SPRING
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+
+    // KOTLIN
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    // ARROW
+    // ARROW
+    implementation("io.arrow-kt:arrow-core:$arrowVersion")
+    implementation("io.arrow-kt:arrow-fx-coroutines:$arrowVersion")
+
+    // TOOLS
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // TESTS
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
+    testImplementation("com.tngtech.archunit:archunit:1.2.0")
+    testImplementation("com.tngtech.archunit:archunit-junit5:1.2.0")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-assertions-arrow:$kotestArrowVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
 }
 
 tasks.withType<KotlinCompile> {
